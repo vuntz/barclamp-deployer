vuntz/barclamp-deployer

commit f2f444abdfb5fdf0af9f7e295ec136be552ab8b5

Fix up bridge slave finding code to skip OVS-created phantom devices.

chef/cookbooks/barclamp/libraries/nic.rb
@@ -609,8 +609,12 @@ class ::Nic
   # Base class for a bridge.  We handle most bridge manipulation via brctl.
   class ::Nic::Bridge < ::Nic
     def slaves
-      ::Dir.entries("#{@nicdir}/brif").reject do |i|
-        i == '.' || i == '..'
+      ::Dir.entries("#{@nicdir}/brif").select do |i|
+        link = File.join("#{@nicdir}/brif",i)
+        # OVS likes to create links to devices that do not really exist.
+        # Skip them.
+        File.symlink?(link) &&
+          File.exists?(File.readlink(link))
       end.map{|i| ::Nic.new(i)}
     end
 

commit 0adb3695e55da533ce0d2c4e9045de867cd60096

Fix finding bridge slaves
Commit f2f444ab broke the detection of bridge slaves: it tried to check
if symlinks were pointing to existing files, but was not expanding the
paths to absolute paths. So the tests were failing because they were
done on relative paths...

Note that we're using File.expand_path (and not File.realpath) to keep
compatibility with ruby 1.8.x.

chef/cookbooks/barclamp/libraries/nic.rb
@@ -614,7 +614,7 @@ class ::Nic
         # OVS likes to create links to devices that do not really exist.
         # Skip them.
         File.symlink?(link) &&
-          File.exists?(File.readlink(link))
+          File.exists?(File.expand_path(File.readlink(link), "#{@nicdir}/brif"))
       end.map{|i| ::Nic.new(i)}
     end
 